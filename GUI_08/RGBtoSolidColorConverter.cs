154c0cf5d4787f6e2c1497f24cfb4d26b20652eb
Viktor Lundsgaard Andersen <vik.lund.and@gmail.com>
2020-02-21 14:33:10 +0100

    Add project files.

---
+++ GUI_08/RGBtoSolidColorConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+
+namespace GUI_08
+{
+    public class RGBtoSolidColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture) =>
+            System.Windows.Media.Color.FromRgb(
+                System.Convert.ToByte(values[0]),
+                System.Convert.ToByte(values[1]),
+                System.Convert.ToByte(values[2]));
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
